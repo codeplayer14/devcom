61cea30b9a81d32f392f4a3adc401e601ac69408
Padmanabh Gupta <padmanabh14@gmail.com>
2018-10-29 23:35:25 +0530

    Profile data check and db push

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,7 +18,7 @@ router.get(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Profile.findOne({ user: req.user.id })
-      .then(user => {
+      .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
           return res.status(404).json(errors);
@@ -47,6 +47,46 @@ router.post(
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
     if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    //Skills  - split into an array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split("");
+    }
+
+    profileFields.social = {};
+
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "The handle already exists";
+            res.status(400).json(errors);
+          }
+
+          new Profile(profileFields).save().then(profile => {
+            res.json(profile);
+          });
+        });
+      }
+    });
   }
 );
 module.exports = router;
