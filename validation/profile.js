09c0a40e1aeeeb8e3d2eb074d8820179c2f62c97
Padmanabh Gupta <padmanabh14@gmail.com>
2018-11-01 06:38:27 +0530

    fixes

--- validation/profile.js
+++ validation/profile.js
@@ -5,6 +5,7 @@ module.exports = function validateProfileInput(data) {
   data.handle = !isEmpty(data.handle) ? data.handle : "";
   data.status = !isEmpty(data.status) ? data.status : "";
   data.skills = !isEmpty(data.skills) ? data.skills : "";
+  errors = {};
 
   if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
     errors.handle = "Handle needs to be between 2 to 40 characters";
@@ -17,32 +18,32 @@ module.exports = function validateProfileInput(data) {
     errors.skills = "Skills are required";
   }
 
-  if (!Validator.isEmpty(data.website)) {
+  if (!isEmpty(data.website)) {
     if (!Validator.isURL(data.website)) {
       errors.website = "Not a valid url";
     }
   }
-  if (!Validator.isEmpty(data.youtube)) {
+  if (!isEmpty(data.youtube)) {
     if (!Validator.isURL(data.youtube)) {
       errors.youtube = "Not a valid url";
     }
   }
-  if (!Validator.isEmpty(data.instagram)) {
+  if (!isEmpty(data.instagram)) {
     if (!Validator.isURL(data.instagram)) {
       errors.instagram = "Not a valid url";
     }
   }
-  if (!Validator.isEmpty(data.twitter)) {
+  if (!isEmpty(data.twitter)) {
     if (!Validator.isURL(data.twitter)) {
       errors.twitter = "Not a valid url";
     }
   }
-  if (!Validator.isEmpty(data.facebook)) {
+  if (!isEmpty(data.facebook)) {
     if (!Validator.isURL(data.facebook)) {
       errors.facebook = "Not a valid url";
     }
   }
-  if (!Validator.isEmpty(data.linkedin)) {
+  if (!isEmpty(data.linkedin)) {
     if (!Validator.isURL(data.linkedin)) {
       errors.linkedin = "Not a valid url";
     }
