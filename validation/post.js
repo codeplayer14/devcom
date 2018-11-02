7369fb6fa86a7e4546721f6d96cd6e2b196c01c4
Padmanabh Gupta <padmanabh14@gmail.com>
2018-11-02 06:38:09 +0530

    Post validation done

---
+++ validation/post.js
@@ -0,0 +1,18 @@
+const Validator = require("validator");
+const isEmpty = require("./isEmpty");
+module.exports = data => {
+  let errors = {};
+  data.text = isEmpty(data.text) ? "" : data.text;
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Post must be between 10 to 300 characters";
+  }
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
